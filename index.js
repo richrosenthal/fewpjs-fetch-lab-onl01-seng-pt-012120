e4e9b834123dfc6899b0ae049a52441bcc64593f
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2020-08-16 16:35:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 function fetchBooks() {
  fetch('https://anapioficeandfire.com/api/books')
   .then(resp => resp.json())
-  .then(json => renderBooks(json));
+  .then(json => console.log(json));
 }
 
 function renderBooks(books) {
