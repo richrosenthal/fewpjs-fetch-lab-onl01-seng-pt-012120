63af998fd464df25eadaa62a9016bd23b7b9c1ec
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2020-08-16 16:37:45 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-function fetchBooks() {
 
-}
+
+function fetchBooks() {
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  .then(json => renderBooks(json));
+}	
 
 function renderBooks(books) {
   const main = document.querySelector('main')
@@ -9,8 +13,10 @@ function renderBooks(books) {
     h2.innerHTML = book.name
     main.appendChild(h2)
   })
+  
 }
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks()
 })
+
